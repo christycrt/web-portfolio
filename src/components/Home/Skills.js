8e1f5bffb60b00e85bb9406b4a29a2ffe5643cc5
christycrt <christycrt@gmail.com>
2022-05-05 01:06:07 +0700

    [ADD] skill icon

--- src/components/Home/Skills.js
+++ src/components/Home/Skills.js
@@ -7,6 +7,13 @@ import {
   FaReact,
   FaSourcetree,
 } from "react-icons/fa";
+import {
+  SiAntdesign,
+  SiNextdotjs,
+  SiStyledcomponents,
+  SiTailwindcss,
+  SiTypescript
+} from "react-icons/si";
 
 const Skills = () => {
   return (
@@ -25,10 +32,25 @@ const Skills = () => {
               <FaJs className="skill-icon skill1" />
             </div>
             <div className="icon-wrapper">
+              <SiTypescript className="skill-icon skill1" />
+            </div>
+            <div className="icon-wrapper">
               <FaReact className="skill-icon skill1" />
             </div>
             <div className="icon-wrapper">
+              <SiNextdotjs className="skill-icon skill1" />
+            </div>
+            <div className="icon-wrapper">
               <FaBootstrap className="skill-icon" />
+            </div>
+            <div className="icon-wrapper">
+              <SiTailwindcss className="skill-icon" />
+            </div>
+            <div className="icon-wrapper">
+              <SiAntdesign className="skill-icon" />
+            </div>
+            <div className="icon-wrapper">
+              <SiStyledcomponents className="skill-icon" />
             </div>
             <div className="icon-wrapper">
               <FaGitAlt className="skill-icon" />
